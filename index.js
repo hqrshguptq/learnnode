a3d1ad98ce6c6bfabd2500bf06e0867f0754968e
Harsh Gupta <harshgupta@Harshs-MacBook-Air.local>
2020-03-28 01:51:11 +0530

    Initial

--- index.js
+++ index.js
@@ -5,9 +5,32 @@ const fs=require('fs');
 
 
 const server=http.createServer((req,res)=>{
-if (req.url==='/'){
-    res.end('<h1>Home</h1>');
-}
+
+
+// if (req.url==='/api/users'){
+//     const users=[
+//         {name:'Harsh',age:20},
+//         {name:'HG'}
+//     ];
+//     res.writeHead(200,{'Content-Type':'application'})
+//     res.end(JSON.stringify(users));
+// }
+// })
+
+// if (req.url==='/'){
+//     fs.readFile(
+//         path.join(__dirname,'public','index.html'),
+//         (err, content) => {
+//      if (err)throw err;       
+//     res.writeHead(200,{'Content-Type':'text/html'})
+//     res.end(content);
+// })
+// }
+
+//Build File Path
+let filePath=path.join(__dirname,'public',req.url==='/'?'index.html':req.url)
+console.log(filePath);
+res.end();
 })
 const PORT=process.env.PORT||5000;
 
