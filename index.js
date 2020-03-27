f2bfe4f923644d110e6f7eb8bbec15a671babfad
Harsh Gupta <harshgupta@Harshs-MacBook-Air.local>
2020-03-28 02:00:25 +0530

    changes

--- index.js
+++ index.js
@@ -17,21 +17,22 @@ const server=http.createServer((req,res)=>{
 // }
 // })
 
-// if (req.url==='/'){
-//     fs.readFile(
-//         path.join(__dirname,'public','index.html'),
-//         (err, content) => {
-//      if (err)throw err;       
-//     res.writeHead(200,{'Content-Type':'text/html'})
-//     res.end(content);
-// })
-// }
+if (req.url==='/'){
+    fs.readFile(
+        path.join(__dirname,'public','index.html'),
+        (err, content) => {
+     if (err)throw err;       
+    res.writeHead(200,{'Content-Type':'text/html'})
+    res.end(content);
+})
+
 
 //Build File Path
-let filePath=path.join(__dirname,'public',req.url==='/'?'index.html':req.url)
-console.log(filePath);
-res.end();
-})
+//let filePath=path.join(__dirname,'public',req.url==='/'?'index.html':req.url)
+
+//Ext of file
+//let extname=path.extname
+}})
 const PORT=process.env.PORT||5000;
 
 server.listen(PORT,()=>console.log(`Server Runnin @ ${PORT}`));
